77cc56b1f5f8c06f2a2181705b9ce28ccca088a3
Aleks Petrov <hello@alekspetrov.com>
2022-10-11 16:29:08 +0200

    Clean up

--- src/router/emails.js
+++ src/router/emails.js
@@ -8,18 +8,11 @@ const addEmail = async request => {
     // eslint-disable-next-line no-undef
     api_key: OCTOPUS_TOKEN,
     email_address: email,
-    fields: {
-      FirstName: '',
-      LastName: '',
-      Birthday: '',
-    },
-    tags: [],
-    status: '',
   })
 
   try {
     const res = await fetch(
-      'https://emailoctopus.com/api/1.5/lists/a803337d-9581-11ec-9258-0241b9615763/contacts',
+      'https://emailoctopus.com/api/1.6/lists/a803337d-9581-11ec-9258-0241b9615763/contacts',
       {
         method: 'POST',
         headers: {
